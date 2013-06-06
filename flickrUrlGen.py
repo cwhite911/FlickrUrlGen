e1ffb5f4358cc353207a927f89bb680f8d5bc362
Corey <ctw911@mac.com>
2013-06-06 16:01:47 -0400

    Changes to how file is written

--- flickrUrlGen.py
+++ flickrUrlGen.py
@@ -1,20 +1,22 @@
 import flickrapi
-import csv
+
 
 api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 flickr = flickrapi.FlickrAPI(api_key)
 photoset = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 def urlGen( photoset_id ):
 	with open('graveurls.csv', 'w') as o:
+		o.write('title, url\n')
 		for photo in flickr.walk_set( photoset_id ):
 			title = photo.get('title')
 			id = photo.get('id')
 			secret = photo.get('secret')
 			server = photo.get('server')
 			farm = photo.get('farm')
-			url = ( title + ',' + 'http://farm' + farm + '.static.flickr.com/' + server + '/' + id + '_' + secret + '_m.jpg')
-			print>>o, url
+			url = ( title + ',' + 'http://farm' + farm + '.static.flickr.com/' + server + '/' + id + '_' + secret + '_m.jpg\n')
+			o.write(url)
 	
 			
 urlGen(photoset)
+
 	
